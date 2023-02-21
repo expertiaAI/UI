1f31608e988c4209259f3d471512ecca4388d872
sanketh <sanket@expertia.ai>
2023-02-21 10:04:50 +0530

    dropdown added

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 export const Input= ({ children,className,...rest }) => {
 
   const combined = twMerge(
-    "border-[1px] border-[#D9DEE2] rounded-lg text-md p-2 focus:border-[#6D5DE7] outline-none border-solid",
+    "border-[1px] border-[#D9DEE2] rounded-lg text-md p-2 focus:border-[#6D5DE7] outline-none border-solid w-full",
     className
   );
 
