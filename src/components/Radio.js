e9004c68299333d4c1d176b085855020f9000532
sanketh <sanket@expertia.ai>
2023-02-22 13:01:54 +0530

    radio updated-2

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -24,9 +24,7 @@ export const Radio = ({ children,color, className, ...rest }) => {
               <RadioGroup.Indicator
                 className={`absolute -left-[12.5%] -top-[12.5%] w-5 h-5 bg-transparent ${color?`text-${color} border-${color}`:'text-[#6D5DE7] border-[#6D5DE7]'} text-[#6D5DE7] rounded-full border-solid border-[2px]  border-[#6D5DE7]`}
               />
-              <RadioGroup.Indicator
-                className={`absolute -left-[75%] -top-[75%] w-10 h-10 ${color? `hover:bg-${color}/10` :'hover:bg-[#6D5DE7]/10'}  rounded-full`}
-              />
+
             </RadioGroup.Item>
             <label className="Label pl-3" htmlFor={i}>
               {e}
