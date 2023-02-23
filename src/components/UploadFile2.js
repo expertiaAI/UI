78bbcf2728c2897df9114a8f08defbe54f5164a0
sanketh <sanket@expertia.ai>
2023-02-23 13:34:55 +0530

    upload comp updated-3

--- src/components/UploadFile2.js
+++ src/components/UploadFile2.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-export const Upload2 = ({ children,className, ...rest }) => {
+export const Upload2 = ({ children, className, ...rest }) => {
   const combined = twMerge(
     `w-full h-36 border border-dashed border-[#6D5DE7] text-center rounded-xl cursor-pointer relative`,
     className
   );
   return (
     <div>
-      <input type="file" className={combined} {...rest} />
+      <div className={combined}>
+        <input type="file" className="hidden" {...rest} />
+      </div>
     </div>
   );
 };
