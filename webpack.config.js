951e25238fa43c5e6de5a548ed65cf64926fed6a
sanketh <sanket@expertia.ai>
2023-03-07 11:28:56 +0530

    wepack update--2

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -10,7 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         libraryTarget: 'umd',
         umdNamedDefine: true,
-        globalObject: 'this',
+        globalObject: 'globalThis',
     },
     externalsPresets: {node: true},
     externals: [nodeExternals()],
