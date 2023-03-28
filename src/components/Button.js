cda13aead7a3f982d59d8a8671fe541a4d7c7fba
sanketh <sanket@expertia.ai>
2023-03-28 12:51:49 +0530

    btn test update-1

--- src/components/Button.js
+++ src/components/Button.js
@@ -16,7 +16,7 @@ export const Button = ({
       secondary
         ? "border border-[#6D5DE7] text-[#6D5DE7] hover:text-[#6559C1] hover:border-[#6559C1] bg-white "
         : " bg-[#6D5DE7] hover:bg-[#6559C1] text-white "
-    }  duration-200 rounded-[4px]  ${
+    }  duration-200 rounded-[4px] sm:bg-red-500 ${
       small
         ? " text-sm px-4 py-2.5 font-medium "
         : " text-md  py-[14px] px-6 font-semibold "
