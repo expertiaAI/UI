e512a35e2f4cb17973eecb3f183bdcb6ed138bb5
sanketh <sanket@expertia.ai>
2023-03-30 00:01:04 +0530

    switch changes

--- src/components/Switches.js
+++ src/components/Switches.js
@@ -1,32 +1,24 @@
 import React from "react";
 import * as Switch from "@radix-ui/react-switch";
-import { useState } from "react";
-import { twMerge } from "tailwind-merge";
 
-export const Switch1 = ({
-  className,
-  switchColor,
-  // defaultChecked,
-  ...rest
-}) => {
-  // let [block, setBlock] = useState(defaultChecked);
-
-  const combined = twMerge(
-    "w-[36px] h-[20px] rounded-3xl",
-    `${
-      block
-        ? `${switchColor ? `bg-${switchColor}` : "bg-[#6D5DE7]"}`
-        : "bg-gray-200"
-    }`,
-    className
-  );
+export const Switch1 = ({...rest}) => {
   return (
-    <div className="flex items-center">
-      <Switch.Root className={combined} {...rest}>
-        <Switch.Thumb
-          className={`w-[16px] h-[16px] rounded-3xl bg-white block translate-x-[2px] duration-200`}
-        />
-      </Switch.Root>
+    <div className=" ">
+      <form>
+        <div
+          className="flex items-center"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <Switch.Root
+            className="w-[36px] h-[20px] bg-gray-300 rounded-full relative  border-[#6D5DE7]  data-[state=checked]:bg-[#6D5DE7] outline-none hover:cursor-pointer"
+            id="airplane-mode"
+            style={{ "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)" }}
+            {...rest}
+          >
+            <Switch.Thumb className="block w-[16px] h-[16px]  border-[#6D5DE7] bg-white rounded-full  transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[18px]" />
+          </Switch.Root>
+        </div>
+      </form>
     </div>
   );
 };
