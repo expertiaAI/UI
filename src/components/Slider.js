6b0fc0e014371b2d55e834e8bb1931f124c2cb3b
sanketh <sanket@expertia.ai>
2023-04-10 18:46:47 +0530

    slider update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import * as Slider from "@radix-ui/react-slider";
 import { twMerge } from "tailwind-merge";
 
-export const Slider1 = ({ children, className, ...rest }) => {
-
-    const combined = twMerge("relative flex items-center w-[500px] rounded-2xl",className)
+export const Slider1 = ({ children, className, value, ...rest }) => {
+    let [trigger,setTrigger] = useState(false)
+  const combined = twMerge(
+    "relative flex items-center w-[500px] rounded-2xl",
+    className
+  );
   return (
     <form>
       <Slider.Root
         className={combined}
-        value={rest.value}
-        defaultValue={[50]}
+        // value={rest.value}
+        // defaultValue={[50]}
         max={100}
         step={1}
         aria-label="Volume"
@@ -19,8 +22,13 @@ export const Slider1 = ({ children, className, ...rest }) => {
         <Slider.Track className="SliderTrack bg-gray-100 relative flex-grow rounded-2xl h-[6px] cursor-pointer">
           <Slider.Range className="SliderRange bg-gray-100 absolute h-[100%] rounded-2xl cursor-pointer" />
         </Slider.Track>
-        <Slider.Thumb className="SliderThumb block h-5 w-5 rounded-xl bg-[#6D5DE7] shadow-md hover:bg-[#574ab8] duration-200 cursor-pointer" />
+
+        <Slider.Thumb onMouseEnter={()=>setTrigger(true)} onMouseLeave={()=>setTrigger(false)} className="SliderThumb block h-5 w-5 rounded-xl bg-[#6D5DE7] shadow-md hover:bg-[#574ab8] duration-200 cursor-pointer relative outline-none">
+          {trigger?<div className="absolute bottom-5 outline-none bg-white shadow-md w-[45px] rounded h-[30px] flex justify-center items-center -translate-y-1 -translate-x-2.5">
+            {value??value[0]}
+          </div>:""}
+        </Slider.Thumb>
       </Slider.Root>
     </form>
   );
-}
+};
