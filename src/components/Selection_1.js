4c14b798ecc8a4038591224c141d33d1025c3bf4
sanketh <sanket@expertia.ai>
2023-04-11 10:27:42 +0530

    selector text changed

--- src/components/Selection_1.js
+++ src/components/Selection_1.js
@@ -9,7 +9,7 @@ export const Selector1 = ({ children, className, defaultChecked,noIcon, ...rest 
   const combined = twMerge(
     "text-sm flex justify-start items-center p-2 pl-3 border-[1px] duration-200 rounded cursor-pointer",
     !selection
-      ? "border-gray-300 text-gray-300 bg-white "
+      ? "border-gray-300 text-[#2B2939] bg-white "
       : "border-[#6D5DE7] text-[#6D5DE7] bg-[#F4F2FF]",
     className
   );
