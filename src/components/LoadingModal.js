4dca48caa600ef942023b53de22163386299afc4
sanketh <sanket@expertia.ai>
2023-04-30 16:09:47 +0530

    loading screen update

--- src/components/LoadingModal.js
+++ src/components/LoadingModal.js
@@ -13,90 +13,61 @@ export const LoadingModal = ({ fetching, setFetching }) => {
         <style>
           {`
          
-.mask {
-	// background: #fff;
-	width: 100%;
-	height: 100%;
-	position: fixed;
-  border-radius: 14px
-}
-.loader {
-  position: relative;
-  width: 2.5em;
-  height: 2.5em;
-  transform: rotate(165deg);
-}
-.loader:before, .loader:after {
-  content: '';
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  display: block;
-  width: 0.5em;
-  height: 0.5em;
-  border-radius: 0.25em;
-  transform: translate(-50%, -50%);
-}
-.loader:before {
-  animation: before 2s infinite;
-}
-.loader:after {
-  animation: after 2s infinite;
-}
-
-@keyframes before {
-  0% {
-    width: 0.5em;
-    box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
-  }
-  35% {
-    width: 2.5em;
-    box-shadow: 0 -0.5em rgba(225, 20, 98, 0.75), 0 0.5em rgba(111, 202, 220, 0.75);
-  }
-  70% {
-    width: 0.5em;
-    box-shadow: -1em -0.5em rgba(225, 20, 98, 0.75), 1em 0.5em rgba(111, 202, 220, 0.75);
-  }
-  100% {
-    box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
-  }
-}
-@keyframes after {
-  0% {
-    height: 0.5em;
-    box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
-  }
-  35% {
-    height: 2.5em;
-    box-shadow: 0.5em 0 rgba(61, 184, 143, 0.75), -0.5em 0 rgba(233, 169, 32, 0.75);
-  }
-  70% {
-    height: 0.5em;
-    box-shadow: 0.5em -1em rgba(61, 184, 143, 0.75), -0.5em 1em rgba(233, 169, 32, 0.75);
-  }
-  100% {
-    box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
-  }
-}
-
-.loader {
-  position: absolute;
-  top: calc(50% - 1.25em);
-  left: calc(50% - 1.25em);
-}
+         *{
+            box-sizing: border-box;
+            margin: 0;
+        }
+        
+        
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #333;
+        
+        }
+        
+        .load{
+            display: flex;
+            justify-content: center;
+            margin: 200px auto;
+        }
+        
+        .load div{
+            width: 20px;
+            height: 20px;
+            background-color: rgb(11, 169, 213);
+            border-radius: 50%;
+            margin: 0 5px;
+            animation-name: up-and-down;
+            animation-duration: 0.8s;
+            animation-iteration-count: infinite;
+            animation-direction: alternate;
+        }
+        
+        .load .two{
+            animation-delay: 0.3s;
+        }
+        
+        .load .three{
+            animation-delay: 0.6s;
+        }
+        
+        @keyframes up-and-down {
+          
+            to{
+                opacity: 0.2;
+                transform: translateY(-20px);
+                
+            }
+        }
           
           `}
         </style>
 
-        <div className="flex flex-col justify-center items-center  p-8 px-12 rounded-md bg-transparent relative  w-[30vw] h-[30vw] ">
-          <div class="mask">
-            <div class="loader"></div>
-          </div>
-          <span className="text-xl text-white font-semibold text-center translate-y-[50px]">
-            {" "}
-            Loading...
-          </span>
-        </div>
+        <div class="load">
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+    </div>
       </>
     </Modal>
   );
