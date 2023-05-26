ff3e8973bb9be9a71a475ab8a21d17ac96a2d8ef
sanketh <sanket@expertia.ai>
2023-05-26 16:23:29 +0530

    added label in input field

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-export const Input= ({ children,className,...rest }) => {
-
+export const Input = ({ label, children, className, ...rest }) => {
   const combined = twMerge(
-    "border-[1px] border-[#D9DEE2] rounded text-[14px] px-4 py-4 focus:border-[#6D5DE7] outline-none border-solid w-full text-sm",
+    `border-[1px] border-[#D9DEE2] ${
+      label ? "rounded-r" : "rounded"
+    } text-[14px] px-4 py-4 focus:border-[#6D5DE7] outline-none border-solid w-full text-sm`,
     className
   );
 
+  console.log(label);
+
   return (
-    <div >
-        <input
-          className={combined}
-          placeholder="Placeholder here"
-          {...rest}
-        />
+    <div className="flex items-center">
+      {label ? (
+        <section
+          className={`bg-[#6D5DE7] text-white px-2 py-4 text-sm border-[1px] border-solid border-[#6D5DE7] ${
+            label ? "rounded-l" : ""
+          } `}
+        >
+          {label}
+        </section>
+      ) : (
+        ""
+      )}
+      <input className={combined} placeholder="Placeholder here" {...rest} />
     </div>
   );
-}
-
+};
