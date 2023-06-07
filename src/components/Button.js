54c835c204b7d4675ee471c4a204c78449ea25aa
sanketh <sanket@expertia.ai>
2023-06-07 13:14:08 +0530

    added loading in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,6 +9,7 @@ export const Button = ({
   secondary,
   disabled,
   icon,
+  loading,
   ...rest
 }) => {
   const combined = twMerge(
@@ -34,8 +35,22 @@ export const Button = ({
   );
 
   return (
-    <button className={combined} {...rest}>
-      {children}
-    </button>
+    <>
+      <button className={combined} {...rest}>
+        {loading ? (
+          <svg
+            className="animate-spin w-full"
+            width="24"
+            height="24"
+            fill={!secondary ? `#FFF` : "#6D5DE7"}
+            viewBox="0 0 256 256"
+          >
+            <path d="M236,128a108,108,0,0,1-216,0c0-42.52,24.73-81.34,63-98.9A12,12,0,1,1,93,50.91C63.24,64.57,44,94.83,44,128a84,84,0,0,0,168,0c0-33.17-19.24-63.43-49-77.09A12,12,0,1,1,173,29.1C211.27,46.66,236,85.48,236,128Z"></path>
+          </svg>
+        ) : (
+          children
+        )}
+      </button>
+    </>
   );
 };
